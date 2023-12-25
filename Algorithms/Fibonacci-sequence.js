6ddee0121cf785fd51b9edc8f62a4b925e2ec42e
iksagor0 <iksagor0@gmail.com>
2023-12-25 12:26:43 +0600

    FIBONACCI SEQUENCE

--- Algorithms/Fibonacci-sequence.js
+++ Algorithms/Fibonacci-sequence.js
@@ -21,3 +21,20 @@ function fibonacciNoAlg(num) {
 }
 
 console.log(fibonacciNoAlg(7));
+
+/********************************************************
+ *           BEST THINK FOR FIBONACCI SEQUENCE
+ ********************************************************/
+
+function fibonacciNoAlg(num) {
+  const result = [0, 1];
+
+  // functionality
+  for (let i = 2; i < num; i++) {
+    result[i] = result[i - 1] + result[i - 2];
+  }
+
+  return result;
+}
+
+console.log(fibonacciNoAlg(8));
